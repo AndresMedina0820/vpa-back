d94d0ffd70c6bd05eb90831bbca822b9157ebe93
amedina <steven@grydd.com>
2022-05-08 00:45:28 -0500

    deploy

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3004;
 const routerApi = require('./routes');
+const cors = require('cors');
 
 app.use(express.json())
+
+const whiteList = ['http://localhost:3004/', 'http://localhost:3000/'];
+const options = {
+	origin: (origin, callback) => {
+		if (whiteList.includes(origin) || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not Allow'), false);
+		}
+	}
+};
+app.use(cors(options));
+
 routerApi(app);
 console.log(port)
 app.listen(port);
