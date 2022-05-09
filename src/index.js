ae37e3ef25e597a579f5194199381a3a437b7646
amedina <steven@grydd.com>
2022-05-09 17:53:31 -0500

    cors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ const express = require('express');
 const app = express();
 const routerApi = require('./routes');
 
+const cors = require('cors');
+
 app.use(express.json())
+
+const whiteList = ['http://localhost:3000/'];
+const options = {
+	origin: (origin, callback) => {
+		if (whiteList.includes(origin) || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not Allow'), false);
+		}
+	}
+};
+app.use(cors(options));
 
 routerApi(app);
 app.listen(process.env.PORT || 3000)
