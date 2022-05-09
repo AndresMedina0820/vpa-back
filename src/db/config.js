26256bf701b5f05c080942f751d62111ff878403
amedina <steven@grydd.com>
2022-05-09 17:35:42 -0500

    deploy

--- src/db/config.js
+++ src/db/config.js
@@ -14,8 +14,10 @@ module.exports = {
 	production: {
 		url: config.dbUrl,
 		dialect: 'postgres',
-		ssl: {
-			rejectUnauthorized: false
+		dialectOptions: {
+			ssl: {
+				rejectUnauthorized: false
+			}
 		}
 	}
 }
