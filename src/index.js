17433061a030a1b05f313adaec20d1de986ceb61
amedina <steven@grydd.com>
2022-05-09 18:33:15 -0500

    cors

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const routerApi = require('./routes');
 
-const cors = require('cors');
-
 app.use(express.json())
 
-const whiteList = ['http://localhost:3000/', 'https://vpa-back.herokuapp.com/'];
-const options = {
-	origin: (origin, callback) => {
-		if (whiteList.includes(origin) || !origin) {
-			callback(null, true);
-		} else {
-			callback(new Error('Not Allow'), false);
-		}
-	}
-};
-app.use(cors(options));
+// const whiteList = ['http://localhost:3000/', 'https://vpa-back.herokuapp.com/'];
+// const options = {
+// 	origin: (origin, callback) => {
+// 		if (whiteList.includes(origin) || !origin) {
+// 			callback(null, true);
+// 		} else {
+// 			callback(new Error('Not Allow'), false);
+// 		}
+// 	}
+// };
+// app.use(cors(options));
+
+const corsOptions ={
+    origin:'http://localhost:3000',
+    credentials:true,
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 routerApi(app);
 app.listen(process.env.PORT || 3000)
