4454f9e829fd0e085e6c102a2629c35608f0320f
amedina <steven@grydd.com>
2022-05-09 09:39:42 -0500

    deploy, change env

--- src/libs/sequelize_connection.js
+++ src/libs/sequelize_connection.js
@@ -8,7 +8,8 @@ const PASSWORD = encodeURIComponent(config.dbPassword);
 /**
  * @var URI is url for connection to db
  */
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://vwuqzhnagweuzd:9dfa5a87ecf4bcb34980acccb9b4faa95dd8fb504cb906086100de666764d983@ec2-35-168-194-15.compute-1.amazonaws.com:5432/d56jfhhmcrabtu`;
 
 const sequelize = new Sequelize(URI, {
 	dialect: config.dialect,
