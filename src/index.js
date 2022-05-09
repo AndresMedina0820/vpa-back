483a7d0ddf48b7ba1f9547287b87552e597e2557
amedina <steven@grydd.com>
2022-05-09 17:13:07 -0500

    chancge ports

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 const routerApi = require('./routes');
 
 app.use(express.json())
 
 routerApi(app);
-console.log(port);
-app.listen(port);
+app.listen(process.env.PORT || 3000)
 
 app.get('/', (request, response) => {
 	response.send('API REST VPA');
