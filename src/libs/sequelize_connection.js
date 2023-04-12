bbfeba9faa732f05ea6c981ebaf929b674745656
AndreDev <andresmedina0820@gmail.com>
2023-04-12 13:20:03 -0500

    👷🏼fix: solved error connection db

--- src/libs/sequelize_connection.js
+++ src/libs/sequelize_connection.js
@@ -2,16 +2,9 @@ const { Sequelize } = require('sequelize');
 const { config } = require('../config/config');
 const setupModels = require('../db/models');
 
-// const USER = encodeURIComponent(config.dbUser);
-// const PASSWORD = encodeURIComponent(config.dbPassword);
-
-// URL de Conexion
-// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-
 const options = {
 	dialect: 'postgres',
-	logging: false,
-	// logging: config.isProd ? false : true,
+	logging: config.isProd ? false : true,
 }
 
 if (config.isProd) {
