b5943f264aac574d5b89aa16030bbe42e38199e3
AndreDev <andresmedina0820@gmail.com>
2023-04-17 19:02:47 -0500

    👷🏼fix: solved error catch router and update envs

--- src/db/config.js
+++ src/db/config.js
@@ -1,8 +1,5 @@
 const { config } = require("../config/config");
 
-// const USER = encodeURIComponent(config.dbUser);
-// const PASSWORD = encodeURIComponent(config.dbPassword);
-
 // URL de Conexion
 const URI = `postgres://${config.dbUser}:${config.dbPassword}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
